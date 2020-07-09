e836947b37a7f5e902b0a18a60d6c61ef0520fb7
ItsMeEnuma <68003616+ItsMeEnuma@users.noreply.github.com>
2020-07-09 18:45:59 +0300

    Delete Main.java

--- task1/src/task1/Main.java
+++
@@ -1,9 +0,0 @@
-package task1;
-
-public class Main {
-    public static void main(String[] args) {
-        Formula f = new Formula();
-        f.prepare("2 * 2 + 2 + 2 * 2");
-        System.out.println(f.execute());
-    }
-}
